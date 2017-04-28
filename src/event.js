f6c1a500dd12aaa98abb34949e53566a5c3bfeb9
matthewdias <matthewdias@me.com>
2017-04-27 23:53:47 -0500

    Add initial route handling

--- src/event.js
+++ src/event.js
@@ -7,9 +7,36 @@ export default async (ctx, next, kitsu) => {
     ctx.body = challenge
     return
   }
-  let { team_id, event } = payload
-  let { message_ts, links } = event
-  console.log('event: ' + 'link_shared: ' + links)
+  let { team_id, event } = ctx.request.body
+  let { channel, message_ts, links } = event
+  console.log('event: ' + 'link_shared: ' + links.map(link => link.url).join())
 
-  // WebClient.
+  let { token } = await getTeam(team_id)
+  let slack = new WebClient(token)
+
+  await Promise.all(links.map(async (link) => {
+    let { url } = link
+    let unfurl
+    if (/anime/.test(url)) {
+      unfurl = { text: 'anime' }
+    } else if (/manga/.test(url)) {
+      unfurl = { text: 'manga' }
+    } else if (/users/.test(url)) {
+      unfurl = { text: 'user' }
+    } else if (/posts/.test(url)) {
+      unfurl = { text: 'post' }
+    } else if (/comments/.test(url)) {
+      unfurl = { text: 'comment' }
+    } else if (/groups/.test(url)) {
+      unfurl = { text: 'group' }
+    } else if (/reviews/.test(url)) {
+      unfurl = { text: 'review' }
+    } else if (/feedback/.test(url)) {
+      unfurl = { text: 'feedback' }
+    } else return
+
+    await slack.chat.unfurl(message_ts, channel, {
+      [url]: unfurl
+    }).catch((err) => { throw new Error(err) })
+  }))
 }
